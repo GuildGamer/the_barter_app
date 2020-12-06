ec39055b96299430f8c50366e44eddafbf160d5e
GuildGamer <tobi4steve@gmail.com>
2020-12-06 10:57:40 -0800

    made UI changes

--- the_barter_app/base_app/urls.py
+++ the_barter_app/base_app/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from .views import (
     HomeView,
+    ShopGridView,
     )
 
 app_name = 'base_app'
 
 urlpatterns = [
      path('', HomeView.as_view(), name='item-list'),
+     path('shop-grid/', ShopGridView.as_view(), name='shop-grid'),
 ]
--- the_barter_app/base_app/views.py
+++ the_barter_app/base_app/views.py
@@ -2,14 +2,22 @@ from django.shortcuts import render
 from .models import Item
 from django.views.generic import ListView, DetailView, View, CreateView
 
-
+#CLASS BASED VIEWS
 class HomeView(ListView):
    model = Item
    template_name = "index.html"
 
-   context = {
+context = {
     'items': Item.objects.all()
 }
-def item_list(request):
 
+class ShopGridView(ListView):
+   model = Item
+   template_name = "shop-grid.html"
+
+#FUNCTION BASED VIEWS
+def item_list(request):
     return render(request, "index.html", context)
+
+
+    
