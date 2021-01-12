03175b912bb0d267e622d93c273ece8673224b23
Goodness Ezeokafor <gootech442@yahoo.com>
2021-01-12 14:06:16 +0100

    hold on

--- base_app/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Item, TradeItem, Inventory, Profile
-
-admin.site.register(Item)
-admin.site.register(TradeItem)
-admin.site.register(Inventory)
-admin.site.register(Profile)
